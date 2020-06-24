9972e7153fe13b17b2d777ce5d707fd6782dc3bb
trthanhson253 <trthanhson253@gmail.com>
2020-06-23 20:26:48 -0400

    first commit

---
+++ public/precache-manifest.6928673fa92c776d7509da591ff95e04.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e06e532e1175409ada3498e85a8679f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f59de81916683ad06668",
+    "url": "/static/css/main.1ed2943f.chunk.css"
+  },
+  {
+    "revision": "c2a651e8299713c8ef29",
+    "url": "/static/js/2.64f55506.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.64f55506.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f59de81916683ad06668",
+    "url": "/static/js/main.86372a8e.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  },
+  {
+    "revision": "308a868bfb986349120d10e74a93afe7",
+    "url": "/static/media/banner.308a868b.jpg"
+  },
+  {
+    "revision": "4bd46ff6529f103be43429c8b37a2012",
+    "url": "/static/media/logo.4bd46ff6.png"
+  }
+]);
---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+// bring routes
+const companyRoutes = require('./routes/companyRoutes');
+const commentRoutes = require('./routes/commentRoutes');
+const authRoutes = require('./routes/authRoutes');
+const requestRoutes = require('./routes/requestRoutes');
+// app
+const app = express();
+const path = require('path');
+// db
+mongoose
+  .connect(process.env.DATABASE_CLOUD, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connected'))
+  .catch((err) => {
+    console.log(err);
+  });
+
+// middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// cors
+if (process.env.NODE_ENV === 'development') {
+  // app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
+  app.use(cors());
+}
+
+app.use(function (req, res, next) {
+  if (req.secure) {
+    // request was via https, so do no special handling
+    next();
+  } else {
+    // request was via http, so redirect to https
+    res.redirect('https://' + req.headers.host + req.url);
+  }
+});
+app.use(express.static(path.join('public')));
+// routes middleware
+app.use('/api', companyRoutes);
+app.use('/api', commentRoutes);
+app.use('/api', authRoutes);
+app.use('/api', requestRoutes);
+
+// port
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
