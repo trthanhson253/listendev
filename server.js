cf6c3017c716f5d788ddfb54cbd406e47b0477ca
trthanhson253 <trthanhson253@gmail.com>
2020-06-23 20:29:06 -0400

    second

--- server.js
+++ server.js
@@ -36,15 +36,15 @@ if (process.env.NODE_ENV === 'development') {
   app.use(cors());
 }
 
-app.use(function (req, res, next) {
-  if (req.secure) {
-    // request was via https, so do no special handling
-    next();
-  } else {
-    // request was via http, so redirect to https
-    res.redirect('https://' + req.headers.host + req.url);
-  }
-});
+// app.use(function (req, res, next) {
+//   if (req.secure) {
+//     // request was via https, so do no special handling
+//     next();
+//   } else {
+//     // request was via http, so redirect to https
+//     res.redirect('https://' + req.headers.host + req.url);
+//   }
+// });
 app.use(express.static(path.join('public')));
 // routes middleware
 app.use('/api', companyRoutes);
